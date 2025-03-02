7b9e8b8bd5bf11366f7dd801d41807fbdff853d0
José Luis Vallejo Ruiz <hublol58@gmail.com>
2025-03-02 12:26:20 -0500

    Login,Signup

--- app/src/main/java/com/example/login/LoginActivity.java
+++ app/src/main/java/com/example/login/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.login;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText it_name;
+    private EditText it_password;
+    private Button btn_login;
+    private TextView tv_logeado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,16 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        it_name = findViewById(R.id.it_name);
+        it_password = findViewById(R.id.it_password);
+        btn_login = findViewById(R.id.btn_login);
+        tv_logeado = findViewById(R.id.tv_logeado);
+
+    }
+
+    public String Login(){
+        return "";
     }
 
 
--- app/src/main/java/com/example/login/SignUpActivity.java
+++ app/src/main/java/com/example/login/SignUpActivity.java
@@ -1,6 +1,9 @@
 package com.example.login;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText ed_name;
+    private EditText ed_password;
+    private EditText ed_email;
+    private Button btn_register2;
+    private TextView tv_registered;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,17 @@ public class SignUpActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ed_name = findViewById(R.id.ed_name);
+        ed_password = findViewById(R.id.ed_password);
+        ed_email = findViewById(R.id.ed_email);
+        btn_register2 = findViewById(R.id.btn_register2);
+        tv_registered = findViewById(R.id.tv_registered);
+
     }
+
+    public String signUp(String ed_name, String ed_email, String ed_password){
+
+        return "s";
+    }
+
 }
