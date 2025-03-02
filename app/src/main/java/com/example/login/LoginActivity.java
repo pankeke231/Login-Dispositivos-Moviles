8a58657919bbe9c518dc768b2b2757cf23cb8bdf
Emerson Albornoz <emersonalbornozalvarez@gmail.com>
2025-03-01 23:05:17 -0500

    Added new activities and main activities

--- app/src/main/java/com/example/login/LoginActivity.java
+++ app/src/main/java/com/example/login/LoginActivity.java
@@ -1,8 +1,6 @@
 package com.example.login;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,33 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Button loginButton = findViewById(R.id.login_b);
-        loginButton.setOnClickListener(v -> openLogin());
-
-        Button signUpButton = findViewById(R.id.signUp_b);
-        signUpButton.setOnClickListener(v -> openSignUp());
     }
 
-    public void openLogin() {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-    }
 
-    public void openSignUp() {
-        Intent intent = new Intent(this, SignUpActivity.class);
-        startActivity(intent);
-    }
 }
